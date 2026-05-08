a6d958b5a652966bce866259aa75d7bc9bffafe5
Aadi Rai <aadi.rai2008@gmail.com>
2026-05-08 22:18:53 +1000

    Add interface function signatures

--- scripts/cards_manager.gd
+++ scripts/cards_manager.gd
@@ -1,1 +1,16 @@
 extends Node
+
+func get_base_nouns() -> Array[Noun]:
+	return []
+
+func get_base_adjectives() -> Array[Adjective]:
+	return []
+
+func fuse_nouns(noun1: Noun, noun2: Noun) -> Noun:
+	return null
+
+func fuse_adjectives(adj1: Adjective, adj2: Adjective) -> Adjective:
+	return null 
+
+func create_card(noun: Noun = null, adjectives: Array[Adjective] = []) -> Card:
+	return null
