f5257642156f7a0385ddc46f19a66633eae3c2a9
npmduy <npmduy@gmail.com>
2026-05-10 00:39:27 +1000

    add bot card selection feature

--- scenes/team_building.gd
+++ scenes/team_building.gd
@@ -4,17 +4,26 @@ const CARD_SCENE_PATH = "res://scenes/team_building/card.tscn"
 
 var card_node_scene
 var turn_count = 0
+var bot_hand_ref
+var card_scene
+var bot_deck: Array[Card] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	bot_hand_ref = $BotHand
+	card_scene = preload(CARD_SCENE_PATH)
 	card_node_scene = preload(CARD_SCENE_PATH)
-	var cards: Array[Card] = []
 	CardsManager.init_deck()
+	var cards: Array[Card] = []
 	var base_nouns: Array[Noun] = CardsManager.draw_nouns()
-	#print(base_nouns)
-	for i in range(base_nouns.size()):
-		cards.append(Card.new(base_nouns[i]))
+	for noun in base_nouns:
+		cards.append(Card.new(noun))
 	$Deck.spawn_cards(cards)
+		
+	base_nouns = CardsManager.draw_nouns()
+	for noun in base_nouns:
+		bot_deck.append(Card.new(noun))
+		
 
 
 func connect_signal(card_manager):
@@ -23,17 +32,50 @@ func connect_signal(card_manager):
 func on_user_submit_turn():
 	if turn_count < 3:
 		turn_count += 1
+		bot_move()
 	if turn_count == 3:
 		$Deck.empty_deck()
 		print('Done phrase')
 		return
 	$Deck.empty_deck()
-	var cards: Array[Card] = []
-	var base_adjs: Array[Adjective] = CardsManager.draw_adjectives()
-	print(base_adjs)
-	for i in range(base_adjs.size()):
-		cards.append(Card.new(Noun.new('Effect'), [base_adjs[i]]))
-	$Deck.spawn_cards(cards)
+	bot_deck.clear()
+	if randi_range(1, 2) == 1:
+		var cards: Array[Card] = []
+		var base_adjs: Array[Adjective] = CardsManager.draw_adjectives()
+		print(base_adjs)
+		for adj in base_adjs:
+			cards.append(Card.new(Noun.new('Effect'), [adj]))
+		$Deck.spawn_cards(cards)
+		
+		base_adjs = CardsManager.draw_adjectives()
+		for adj in base_adjs:
+			bot_deck.append(Card.new(Noun.new('Effect'), [adj]))
+	else:
+		var cards: Array[Card] = []
+		var base_nouns: Array[Noun] = CardsManager.draw_nouns()
+		for noun in base_nouns:
+			cards.append(Card.new(noun))
+		$Deck.spawn_cards(cards)
+			
+		base_nouns = CardsManager.draw_nouns()
+		for noun in base_nouns:
+			bot_deck.append(Card.new(noun))
+
+func bot_move():
+	if bot_deck[0].get_noun_str() == "Effect":
+		var idx = randi_range(0, bot_hand_ref.cards.size() - 1)
+		bot_hand_ref.cards[idx].the_card.add_adjective(bot_deck[0].adjectives[0])
+		bot_hand_ref.cards[idx].set_attribute(bot_hand_ref.cards[idx].the_card)
+	else:
+		add_to_bot_hand(bot_deck[0])
+
+func add_to_bot_hand(card: Card):
+	var card_node = card_scene.instantiate()
+	$CardManager.add_child(card_node)
+	card_node.set_attribute(card)
+	card_node.name = "card"
+	card_node.holder = self
+	bot_hand_ref.add_card(card_node)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
