32dcbfd06ee3f2407c1e21e39fab5d1bbe9300ad
trumgbergh <68943610+trumgbergh@users.noreply.github.com>
2026-05-09 12:46:43 +1000

    add health and moving animation

--- scripts/grid_manager.gd
+++ scripts/grid_manager.gd
@@ -22,13 +22,15 @@ func get_pixel_position_from_grid(grid_x: int, grid_y: int) -> Vector2:
 
 func spawn_character(character: Dictionary) -> void:
 	var char_marker: MapMarker = marker_scene.instantiate() as MapMarker
+	var character_id: String = character["id"]
 	var character_x: int = character["x"]
 	var character_y: int = character["y"]
 	var character_z: int = character["z"]
 	if char_marker == null:
 		push_error("Failed to instantiate MapMarker :(")
 		return
-	
+
+	char_marker.name = character_id
 	marker_container.add_child(char_marker)
 	if character["side"] == "Team A":
 		char_marker.set_ring_color(Color.BLUE)
@@ -46,6 +48,7 @@ func spawn_object(object: Dictionary) -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var character: Dictionary = {
+		"id": "a",
 		"max_hp": 100,
 		"hp": 67,
 		"side": "Team A",
@@ -54,7 +57,45 @@ func _ready() -> void:
 		"z": 9,
 	}
 	call_deferred("spawn_character", character)
+	await get_tree().create_timer(2.0).timeout
+	move_character(character, 5, 5)
 
+"""
+moves: Array of Character Objects
+"""
+func move_characters_simultaneously(moves: Array) -> void:
+	var board_tween: Tween = create_tween()
+	board_tween.set_parallel(true)
+	board_tween.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	
+	var universal_move_time: float = 0.5
+	
+	for move in moves:
+		var target_id: String = move["id"]
+		var target_grid_x: int = move["x"]
+		var target_grid_y: int = move["y"]
+		
+		var marker: MapMarker = marker_container.get_node_or_null(target_id) as MapMarker
+		
+		if marker != null:
+			var target_pixel_pos: Vector2 = get_pixel_position_from_grid(target_grid_x, target_grid_y)
+			board_tween.tween_property(marker, "position", target_pixel_pos, universal_move_time)
+		else:
+			print("Tried to move marker, but ID wasn't found: " + target_id)
+
+func move_character(character: Dictionary, target_x: int, target_y: int) -> void:
+	var target_id: String = character["id"]
+	var marker: MapMarker = marker_container.get_node_or_null(target_id) as MapMarker
+	if marker == null:
+		print("Tried to move marker, but ID wasn't found: " + target_id)
+		return
+	
+	var target_pixel_pos: Vector2 = get_pixel_position_from_grid(target_x, target_y)
+	var move_tween: Tween = create_tween()
+
+	move_tween.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	var move_time: float = 0.5
+	move_tween.tween_property(marker, "position", target_pixel_pos, move_time)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scripts/map_marker.gd
+++ scripts/map_marker.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var portrait: Sprite2D = $Visual/Portrait
 @onready var placeholder: Label = $Visual/Placeholder
 @onready var health_ring: TextureProgressBar = $Visual/HealthRing
+var health_tween: Tween
 
 var target_portrait_size: float = 85.0
 # Called when the node enters the scene tree for the first time.
@@ -24,6 +25,15 @@ func apply_dynamic_portrait(new_texture: Texture2D):
 func set_health(current_hp: int, max_hp: int) -> void:
 	health_ring.max_value = max_hp
 	health_ring.value = current_hp
+
+func take_damage(damage_amount: int) -> void:
+	var target_health: int = clampi(int(health_ring.value) - damage_amount, 0, int(health_ring.max_value))
+	if health_tween and health_tween.is_valid():
+		health_tween.kill()
+	
+	health_tween = create_tween()
+	health_tween.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	health_tween.tween_property(health_ring, "value", target_health, 0.4)
 
 func set_ring_color(new_color: Color) -> void:
 	health_ring.tint_progress = new_color
