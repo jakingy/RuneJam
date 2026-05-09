06a85b280dc3ea7b529a17f6113a2b5ea2f2d821
mac <mac@6a-46-17-48-b8-50.walkup.net.uq.edu.au>
2026-05-09 23:02:12 +1000

    Add the UI dashboard scene

---
+++ scripts/battle_ui.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+@onready var player_prob_label = $MarginContainer/HBoxContainer/PlayerStats/Score
+@onready var opp_prob_label = $MarginContainer/HBoxContainer/OppStats/Score
+
+func upd_player_prob(cur: int):
+	player_prob_label.text = "Probability score: %d" % cur
+	
+func upd_opp_prob(cur: int):
+	opp_prob_label.text = "Probability score: %d" % cur
